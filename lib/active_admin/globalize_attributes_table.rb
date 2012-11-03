f023972db030a4f8908464b3b7bd1e815f5f26b4
Gullit Miranda <gullitmiranda@requestdev.com.br>
2012-11-03 03:50:46 -0400

    Fix bug "ActiveAdmin::Views::AttributesTable";

--- lib/active_admin/globalize_attributes_table.rb
+++ lib/active_admin/globalize_attributes_table.rb
@@ -1,35 +1,36 @@
 module ActiveAdmin
   module Views
-    class GlobalizeAttributesTable < ActiveAdmin::Views::AttributesTable
-      builder_method :globalize_attributes_table_for
+    if defined?(::ActiveAdmin::Views::AttributesTable)
+      class GlobalizeAttributesTable < ActiveAdmin::Views::AttributesTable
+        builder_method :globalize_attributes_table_for
 
-      def row(attr, &block)
-        I18n.available_locales.each_with_index do |locale, index|
-          @table << tr do
-            if index == 0
-              th :rowspan => I18n.available_locales.length do
-                header_content_for(attr)
+        def row(attr, &block)
+          I18n.available_locales.each_with_index do |locale, index|
+            @table << tr do
+              if index == 0
+                th :rowspan => I18n.available_locales.length do
+                  header_content_for(attr)
+                end
               end
-            end
-            td do
-              I18n.with_locale locale do
-                content_for(block || attr)
+              td do
+                I18n.with_locale locale do
+                  content_for(block || attr)
+                end
               end
             end
           end
         end
+
+        protected
+
+        def default_id_for_prefix
+          'attributes_table'
+        end
+
+        def default_class_name
+          'attributes_table'
+        end
       end
-
-      protected
-
-      def default_id_for_prefix
-        'attributes_table'
-      end
-
-      def default_class_name
-        'attributes_table'
-      end
-
     end
   end
 end
